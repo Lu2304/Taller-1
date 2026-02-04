6174b7b72166dc90172ccb054b754cda996d4df7
Luisa Fernanda Vargas Aponte <asiul2304@gmail.com>
2026-02-04 09:35:26 -0500

    Creación de scripts hijos e importación de datos

--- Code/00_main.R
+++ Code/00_main.R
@@ -1,6 +1,7 @@
 rm(list = ls())
 
-pkgs = c("tidyverse", "httr", "readr", "fs", "here")
+# Bucle para que R busque los paquetes requridos y si no está instalado, instalarlo y después cargarlo.
+pkgs <- c("rio", "tidyverse")
 
 for (i in pkgs) {
   if(!requireNamespace(i)) {
@@ -9,3 +10,9 @@ for (i in pkgs) {
   library(i, character.only = TRUE)
 }
 
+# Importar datos (primer conjunto)
+GEIH_BOG_01 <- import("https://github.com/ignaciomsarmiento/datasets/blob/main/GEIH_sample1.Rds?raw=true")
+GEIH_BOG_01 <- as_tibble(GEIH_BOG_01)
+
+# Inspeccionar los datos
+source("01_inspecting.R")
