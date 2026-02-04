6a876eb319682749b212e25df551212b546217f1
Luisa Fernanda Vargas Aponte <asiul2304@gmail.com>
2026-02-04 16:15:27 -0500

    Exploración de la base y variables de interés

--- Code/01_inspecting.R
+++ Code/01_inspecting.R
@@ -3,3 +3,28 @@ head(GEIH_BOG_01)
 tail(GEIH_BOG_01)
 glimpse(GEIH_BOG_01)
 skim(GEIH_BOG_01) |> head()
+
+# Análisis de variables de interés
+unique(GEIH_BOG_01$sex)
+unique(GEIH_BOG_01$age)
+unique(GEIH_BOG_01$relab)
+unique(GEIH_BOG_01$y_total_m)
+unique(GEIH_BOG_01$totalHoursWorked)
+unique(GEIH_BOG_01$p6210)
+unique(GEIH_BOG_01$sizeFirm)
+unique(GEIH_BOG_01$ocu)
+
+des_vars <- c("age", "y_total_m", "totalHoursWorked")
+stargazer(as.data.frame(GEIH_BOG_01[, des_vars]), type = "text")
+
+sum(is.na(GEIH_BOG_01$sex))
+sum(is.na(GEIH_BOG_01$age))
+sum(is.na(GEIH_BOG_01$relab))
+sum(is.na(GEIH_BOG_01$y_total_m))
+sum(is.na(GEIH_BOG_01$totalHoursWorked))
+sum(is.na(GEIH_BOG_01$p6210))
+sum(is.na(GEIH_BOG_01$sizeFirm))
+sum(is.na(GEIH_BOG_01$ocu))
+
+
+
