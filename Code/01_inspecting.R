2c86648eafd59f93622e5ef9abf05a8ac013daa0
Luisa Fernanda Vargas Aponte <asiul2304@gmail.com>
2026-02-05 11:13:06 -0500

    ajustar detalles de inspección

--- Code/01_inspecting.R
+++ Code/01_inspecting.R
@@ -4,27 +4,76 @@ tail(GEIH_BOG_01)
 glimpse(GEIH_BOG_01)
 skim(GEIH_BOG_01) |> head()
 
-# Análisis de variables de interés
-unique(GEIH_BOG_01$sex)
-unique(GEIH_BOG_01$age)
-unique(GEIH_BOG_01$relab)
-unique(GEIH_BOG_01$y_total_m)
-unique(GEIH_BOG_01$totalHoursWorked)
-unique(GEIH_BOG_01$p6210)
-unique(GEIH_BOG_01$sizeFirm)
-unique(GEIH_BOG_01$ocu)
+# Definir variables de interés
+vars_interes <- c("sex", "age", "relab", "y_total_m", "ocu", "totalHoursWorked",
+                  "p6210", "sizeFirm")
 
-des_vars <- c("age", "y_total_m", "totalHoursWorked")
+# Resunem estadística descriptiva
+des_vars <- c("sex", "age", "relab","y_total_m", "ocu","totalHoursWorked",
+              "p6210", "sizeFirm")
+
 stargazer(as.data.frame(GEIH_BOG_01[, des_vars]), type = "text")
 
-sum(is.na(GEIH_BOG_01$sex))
-sum(is.na(GEIH_BOG_01$age))
-sum(is.na(GEIH_BOG_01$relab))
-sum(is.na(GEIH_BOG_01$y_total_m))
-sum(is.na(GEIH_BOG_01$totalHoursWorked))
-sum(is.na(GEIH_BOG_01$p6210))
-sum(is.na(GEIH_BOG_01$sizeFirm))
-sum(is.na(GEIH_BOG_01$ocu))
+# Revisar valores únicos 
+tabla_valores_unicos <- GEIH_BOG_01 |> 
+  select(all_of(vars_interes)) |> 
+  summarise(across(
+    everything(),
+    ~ n_distinct(.x, na.rm = TRUE)
+  )) |> 
+  pivot_longer(
+    cols = everything(),
+    names_to = "variable",
+    values_to = "n_valores_unicos"
+  )
 
+tabla_valores_unicos
 
+unique(GEIH_BOG_01$sex)
+unique(GEIH_BOG_01$relab)
+unique(GEIH_BOG_01$ocu)
+unique(GEIH_BOG_01$p6210)
+unique(GEIH_BOG_01$sizeFirm)
 
+# Revisar NAs. Vemos que las variables con más proporción de NAs son:
+# el ingreso laboral, el tipo de trabajo, las horas trabajadas y el
+# tamaño de la firma. 
+
+tabla_na <- GEIH_BOG_01 |> 
+  select(all_of(vars_interes)) |> 
+  summarise(across(
+    everything(),
+    ~ mean(is.na(.x)) * 100
+  )) |> 
+  pivot_longer(
+    cols = everything(),
+    names_to = "variable",
+    values_to = "porcentaje_NA"
+  ) |> 
+  arrange(desc(porcentaje_NA))
+
+tabla_na
+
+# Ver correlación entre los NAs de las variables. De la gráfica se puede 
+# observar que todas las correlaciones son positivas dado el color azul. 
+# Además, el tamaño del círculo ayuda a entender la magnitud de la 
+# correlación. Los valores faltantes de relab están altamente correlacionados
+# positivamente con los ingresos laborales, y ,especialmente, con las
+# horas trabajadas y el tamaño de la firma. Lo cual tiene sentido
+# dado que no se observó el tipo de trabajo y tampoco ingresos laborales, 
+# horas trabajadas o tamaño de la empresa. Lo que puede significar que esa persona
+# no está ocupada. Los ingresos laborales están correlacionados con horas
+# trabajadas, tipo de trabajo y tamaño de la firma por la razón explicada 
+# anteriormente.
+
+vars_interes_NA <- GEIH_BOG_01 |> 
+  select(all_of(vars_interes))
+
+vars_miss <-vars_interes_NA |> 
+  mutate(across(everything(), ~ ifelse(is.na(.x), 1, 0)))
+
+vars_miss <- vars_miss |> 
+  select(which(apply(vars_miss, 2, sd) > 0))
+
+mcorrmiss <- cor(vars_miss)
+corrplot(mcorrmiss)
