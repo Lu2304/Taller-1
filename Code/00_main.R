d775a5816b0163f352a600b947d09a22908d0ed1
Luisa Fernanda Vargas Aponte <asiul2304@gmail.com>
2026-02-05 11:12:09 -0500

    Actualizar paquetes

--- Code/00_main.R
+++ Code/00_main.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 
 # Bucle para que R busque los paquetes requridos y si no está instalado, instalarlo y después cargarlo.
-pkgs <- c("rio", "tidyverse", "skimr", "stargazer")
+pkgs <- c("rio", "tidyverse", "skimr", "stargazer", "dplyr", "tidyr", "corrplot")
 
 for (i in pkgs) {
   if(!requireNamespace(i)) {
